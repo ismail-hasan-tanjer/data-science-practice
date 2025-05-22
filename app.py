6d07fb04401c07e81a1a78b44d9bae159c3d2f15
ismail-hasan-tanjer <tanjerinfo@gmail.com>
2025-05-22 08:24:41 +0600

    Data Cleaning & Preprocessing

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
 #this data science practice file 
+
+#Data Cleaning & Preprocessing
+
+#Data Cleaning & Preprocessing is one of the most important and initial steps of any data science project. The main objective of this step is to prepare the raw data for analysis or modeling. The main steps are given below in detail:
+
