a469862fc2be039ebebe5a5cd72cb000bc1196e6
ismail-hasan-tanjer <tanjerinfo@gmail.com>
2025-05-22 08:28:30 +0600

    Handling Missing Values

--- app.py
+++ app.py
@@ -4,3 +4,14 @@
 
 #Data Cleaning & Preprocessing is one of the most important and initial steps of any data science project. The main objective of this step is to prepare the raw data for analysis or modeling. The main steps are given below in detail:
 
+'''1. Handling Missing Values
+Sometimes some fields in the dataset are empty (null/NaN). These need to be managed:
+
+Drop missing values: df.dropna()
+
+Fill with mean/median/mode: df.fillna(df['column'].mean())
+
+Forward/Backward Fill: df.fillna(method='ffill') or bfill'''
+
+
+
