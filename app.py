f3b74761aa4a32020664d9f5f99a72fb89f4c43c
ismail-hasan-tanjer <tanjerinfo@gmail.com>
2025-10-18 23:38:12 +0600

    05_streamlit_deployment

--- app.py
+++ app.py
@@ -633,6 +633,30 @@ disp.plot(cmap='Blues')
 plt.title("Confusion Matrix")
 plt.show()
 
+🔹 05_streamlit_deployment.ipynb
+
+# Save as `app.py` if you want to run it with Streamlit
+import streamlit as st
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+st.title("🌸 Iris Flower Prediction App")
+
+# User inputs
+sepal_length = st.slider("Sepal Length", 4.0, 8.0, 5.0)
+sepal_width = st.slider("Sepal Width", 2.0, 4.5, 3.0)
+petal_length = st.slider("Petal Length", 1.0, 7.0, 4.0)
+petal_width = st.slider("Petal Width", 0.1, 2.5, 1.2)
+
+# Load dataset & train model
+df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+X = df.drop('species', axis=1)
+y = df['species']
+model = RandomForestClassifier().fit(X, y)
+
+# Prediction
+pred = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+st.write("### 🌼 Predicted Species:", pred[0])
 
 
 
